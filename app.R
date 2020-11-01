1d1e1a7af2a95b22a85c992792f0e71121d427e5
Fahad Taimur <ftaimur1990@gmail.com>
2020-11-01 00:31:11 -0400

    App draft

---
+++ app.R
@@ -0,0 +1,235 @@
+library(shiny)
+library(tidyverse)
+library(plotly)
+library(shinythemes)
+library(shinyjs)
+library(tidyquant)
+library(ggthemes)
+library(shinyWidgets)
+library(DT)
+library(data.table)
+library(quantmod)
+
+source(file="functions.R")
+
+
+# Define UI for application that draws a histogram
+ui <- navbarPage(
+
+    # Title
+    title = "Stock Price - Monte-Carlo Simulation",
+    inverse = FALSE,
+    collapsible = TRUE,
+    
+    theme = shinytheme("united"),
+    
+    tabPanel(
+        # 1.0 Header
+        title = "Simulator",
+        
+        # 2.0 UI Application
+        div(
+            class = "container",
+            
+            # spacing from the top
+            column(width = 12,
+                   div(
+                       p(), br(), p()
+                   )), 
+            
+            column(
+                width = 4, 
+                # wellPanel 
+                shiny::wellPanel(
+                    
+                    # stock picker
+                    pickerInput(inputId = "stock_selection", 
+                                label   = "Select A Stock", 
+                                choices = c("AAPL", "NVDA", "TSLA", "SQ", "NIO", "SPY", "BND"), 
+                                multiple = FALSE,
+                                selected = "TSLA", 
+                                options = pickerOptions(
+                                    actionsBox = FALSE,
+                                    liveSearch = TRUE,
+                                    size=10
+                                )),
+                    
+                    br(),
+                    
+                    # Date from
+                    dateInput(inputId = "date1",
+                              label = "Select Beginning Date",
+                              value = as.Date("2020-01-01")),
+                    
+                    
+                    # Date to
+                    dateInput(inputId = "date2",
+                              label = "Select Ending Date", 
+                              value = Sys.Date()),
+                    
+                    # Expected Volatility
+                    textInput(inputId = "volatility", 
+                              label = "Enter expected volatility", 
+                              value = "0.3"),
+                    
+                    
+                    
+                    # Number of Simulations
+                    pickerInput(inputId = "trials", 
+                                label   = "Select Number of Trials", 
+                                choices = c(10, 50, 100, 500, 1000, 5000, 10000), 
+                                multiple = FALSE,
+                                selected = 1000, 
+                                options = pickerOptions(
+                                    actionsBox = FALSE,
+                                    liveSearch = TRUE,
+                                    size=10
+                                )),
+                    
+                    
+                )
+            ),
+            
+            
+            
+            # Table / Plot Area ---
+            column(
+                width = 8,
+                
+                div(
+                    # Text Above
+                    div(class="text-center", h4("Monte-Carlo Simulation Results")),
+                    tabsetPanel(id="display", 
+                                
+                                
+                                tabPanel(title = "Histogram", 
+                                         plotlyOutput(outputId = "plotly_plot")), 
+                                
+                                
+                                tabPanel(title = "All Results",
+                                         dataTableOutput("Table")
+                                ), 
+                                
+                                tabPanel(title = "Summarized Results", 
+                                         dataTableOutput("Simulation_Summary"))
+                                
+                    )
+                )
+            )
+            
+        )
+    )
+)
+
+# Define server logic required to draw a histogram
+server <- function(input, output) {
+    
+    # Event Reactivity Setup ----
+    
+    ## Event Reactive for Stock Selection
+    stock_selection_reactive <- eventReactive(eventExpr = input$analyze, {
+        input$stock_selection
+    }, ignoreNULL = FALSE)
+    
+    ## Event Reactive - Date From
+    date1_reactive <- eventReactive(eventExpr = input$analyze, {
+        input$date1
+    }, ignoreNULL = FALSE)
+    
+    ## Event Reactive - Date To
+    date2_reactive <- eventReactive(eventExpr = input$analyze, {
+        input$date2
+    }, ignoreNULL = FALSE)
+    
+    ## Event Reactive - Monte-Carlo Trials
+    mc_trials <- eventReactive(eventExpr = input$analyze, {
+        input$trials
+    }, ignoreNULL = FALSE)
+    
+    
+    # Get Stock Data / Analyze
+    data <- reactive({
+        getSymbols(input$stock_selection, src="yahoo", 
+                   from = input$date1,
+                   to = input$date2, 
+                   auto.assign = FALSE) %>%
+            as.data.frame() %>%
+            janitor::clean_names() %>%
+            rownames_to_column(var = "date") %>%
+            mutate(date = lubridate::ymd(date)) %>%
+            select(date, contains("adjusted")) %>%
+            set_names("Date", "Adjusted")
+    })
+    
+    ## Average Price
+    average_price <- reactive({
+        data() %>%
+            summarise(average_price = mean(Adjusted)) %>%
+            pull()
+        })
+    
+    ## Standard Deviation
+    volatility <- reactive({
+        data() %>%
+            summarise(vol = sd(Adjusted)) %>%
+            pull()
+    })
+    
+    ## Rate of Return
+    ror <- reactive({
+        
+        ((data() %>%
+            select("Adjusted") %>%
+            slice(length(Adjusted)) %>% 
+             pull()) / (data() %>%
+            select("Adjusted") %>%
+            slice(1) %>% pull())-1)
+
+    })
+    
+    # Perform Monte-Carlo Simulation
+    stock_simulation <- reactive(
+        replicate(n = input$trials, 
+                  stock_price_model(cp = average_price(),    # convert to daily mean
+                                    days = 252,              # Yearly Days of trading
+                                    gr = ror(),              # % return based on selected dates
+                                    vol = as.numeric(input$volatility))
+                  )
+    )
+    
+    
+    # Table to show simulation results
+    output$Table <- renderDataTable(stock_simulation() %>% 
+                                        tibble() %>% 
+                                        set_names(nm = "Results") %>% 
+                                        mutate(Results = round(Results, digits = 0)))
+    
+    # Table to show simulation summary
+    output$Simulation_Summary <- renderDataTable(
+        stock_simulation() %>%
+            tibble() %>%
+            set_names(nm = "Results") %>%
+            summarise(
+                AveragePrice = mean(Results), 
+                StdError = sd(Results)/sqrt(n()), 
+                StdDeviation = sd(Results),
+                Minimum = min(Results),
+                Maximum = max(Results), 
+                `10th_Percentile` = quantile(Results, probs = 0.1),
+                `90th_Percentile` = quantile(Results, probs = 0.9)
+            ) %>%
+            map_df(.x = ., .f = ~round(., digits = 1))
+    )
+    
+    # Plot - Histogram
+    output$plotly_plot <- renderPlotly({
+        hist_plotter(df=stock_simulation(), current_price = average_price())
+    })
+    
+    
+    
+    
+}
+
+# Run the application 
+shinyApp(ui = ui, server = server)
---
+++ functions.R
@@ -0,0 +1,43 @@
+# Monte-Carlo Simulation
+
+stock_price_model <- function(cp, days, gr, vol){
+  
+  # cp - current price  / days - annual trading days / gr - growth rate / vol - annual volatility
+  
+  # calculate the daily mean 
+  daily_mean <- (gr - 0.5 * (vol^2)) / days
+  daily_std <- vol/sqrt(days)
+  
+  # the price at the beginning (current price)
+  begin_price <- cp
+  
+  # stock price in 3 months
+  for(i in 1:90){
+    # generate normally distributed random number
+    growth_factor <- rnorm(n = 1, mean = daily_mean, sd = daily_std)
+    # calculate ending price
+    end_price <- exp(growth_factor) * begin_price
+    # set the next beginning price to the latest ending price
+    begin_price <- end_price
+  }
+  
+  # return the ending price at the end of the trading
+  return(end_price)
+  
+}
+
+hist_plotter <- function(df, current_price){
+  plotly::ggplotly(
+    df %>%
+      as.tibble() %>%
+      mutate(label = case_when(
+        value > current_price ~ "Buy",
+        TRUE ~ "Not Buy"
+      )) %>%
+      ggplot(aes(value, fill=label)) + 
+      geom_histogram(show.legend = FALSE, bins = 50) +
+      theme_bw() +
+      geom_vline(aes(xintercept=mean(value))) +
+      labs(x = "Price of Stock", title = "Stock Price Estimates")
+  )
+}

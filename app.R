bbd1457c024b6fc6db5ef20407419d892aa0d6f6
Fahad Taimur <ftaimur1990@gmail.com>
2020-11-01 14:03:10 -0500

    Finalized app

--- app.R
+++ app.R
@@ -3,12 +3,12 @@ library(tidyverse)
 library(plotly)
 library(shinythemes)
 library(shinyjs)
-library(tidyquant)
 library(ggthemes)
 library(shinyWidgets)
 library(DT)
 library(data.table)
 library(quantmod)
+library(timetk)
 
 source(file="functions.R")
 
@@ -21,20 +21,20 @@ ui <- navbarPage(
     inverse = FALSE,
     collapsible = TRUE,
     
-    theme = shinytheme("united"),
+    theme = shinytheme("cerulean"),
     
     tabPanel(
-        # 1.0 Header
+        # Header ----
         title = "Simulator",
         
-        # 2.0 UI Application
+        # UI Application ----
         div(
             class = "container",
             
             # spacing from the top
             column(width = 12,
                    div(
-                       p(), br(), p()
+                       br()
                    )), 
             
             column(
@@ -45,7 +45,7 @@ ui <- navbarPage(
                     # stock picker
                     pickerInput(inputId = "stock_selection", 
                                 label   = "Select A Stock", 
-                                choices = c("AAPL", "NVDA", "TSLA", "SQ", "NIO", "SPY", "BND"), 
+                                choices = c("AAPL", "NVDA", "SQ", "SPY", "BND"), 
                                 multiple = FALSE,
                                 selected = "TSLA", 
                                 options = pickerOptions(
@@ -54,7 +54,6 @@ ui <- navbarPage(
                                     size=10
                                 )),
                     
-                    br(),
                     
                     # Date from
                     dateInput(inputId = "date1",
@@ -70,7 +69,7 @@ ui <- navbarPage(
                     # Expected Volatility
                     textInput(inputId = "volatility", 
                               label = "Enter expected volatility", 
-                              value = "0.3"),
+                              value = "0.5"),
                     
                     
                     
@@ -86,13 +85,25 @@ ui <- navbarPage(
                                     size=10
                                 )),
                     
+                    # Number of Simulations
+                    pickerInput(inputId = "forecast_days", 
+                                label   = "Select Forecast Days", 
+                                choices = c(15, 30, 60), 
+                                multiple = FALSE,
+                                selected = 30, 
+                                options = pickerOptions(
+                                    actionsBox = FALSE,
+                                    liveSearch = TRUE,
+                                    size=10
+                                )),
+                    
                     
                 )
             ),
             
             
             
-            # Table / Plot Area ---
+            # Table / Plot Area ----
             column(
                 width = 8,
                 
@@ -105,6 +116,12 @@ ui <- navbarPage(
                                 tabPanel(title = "Histogram", 
                                          plotlyOutput(outputId = "plotly_plot")), 
                                 
+                                tabPanel(title = "Density Plot", 
+                                         plotlyOutput(outputId = "plotly_plot_density")), 
+                                
+                                tabPanel(title = "Time Series Plot", 
+                                         plotlyOutput(outputId = "time_series_plot")), 
+                                
                                 
                                 tabPanel(title = "All Results",
                                          dataTableOutput("Table")
@@ -115,7 +132,28 @@ ui <- navbarPage(
                                 
                     )
                 )
+            ), 
+            
+            # Details ----
+            div(
+                class = "container",
+                id = "summary",
+                column(
+                    width = 12,
+                    div(
+                        class = "panel",
+                        div(class = "panel-header", h4("FYI")),
+                        div(
+                            class = "panel-body",
+                            style = "color: #ffffff; background-color: #3e3f3a;",
+                            p("Utilize the input selections on the left panel to modify the settings for the Monte-Carlo simulation. 
+                              The average price and growth rate for the stock is calculated based on the starting and ending dates. The default
+                              volatility is 50%, feel free to adjust higher or lower as you please.")
+                        )
+                    )
+                )
             )
+            
             
         )
     )
@@ -126,28 +164,8 @@ server <- function(input, output) {
     
     # Event Reactivity Setup ----
     
-    ## Event Reactive for Stock Selection
-    stock_selection_reactive <- eventReactive(eventExpr = input$analyze, {
-        input$stock_selection
-    }, ignoreNULL = FALSE)
     
-    ## Event Reactive - Date From
-    date1_reactive <- eventReactive(eventExpr = input$analyze, {
-        input$date1
-    }, ignoreNULL = FALSE)
-    
-    ## Event Reactive - Date To
-    date2_reactive <- eventReactive(eventExpr = input$analyze, {
-        input$date2
-    }, ignoreNULL = FALSE)
-    
-    ## Event Reactive - Monte-Carlo Trials
-    mc_trials <- eventReactive(eventExpr = input$analyze, {
-        input$trials
-    }, ignoreNULL = FALSE)
-    
-    
-    # Get Stock Data / Analyze
+    # Get Stock Data / Analyze ----
     data <- reactive({
         getSymbols(input$stock_selection, src="yahoo", 
                    from = input$date1,
@@ -187,24 +205,25 @@ server <- function(input, output) {
 
     })
     
-    # Perform Monte-Carlo Simulation
+    # Perform Monte-Carlo Simulation ----
     stock_simulation <- reactive(
         replicate(n = input$trials, 
                   stock_price_model(cp = average_price(),    # convert to daily mean
                                     days = 252,              # Yearly Days of trading
                                     gr = ror(),              # % return based on selected dates
-                                    vol = as.numeric(input$volatility))
+                                    vol = as.numeric(input$volatility), 
+                                    forecast_days = input$forecast_days)
                   )
     )
     
     
-    # Table to show simulation results
+    # Table to show simulation results ---- 
     output$Table <- renderDataTable(stock_simulation() %>% 
                                         tibble() %>% 
                                         set_names(nm = "Results") %>% 
                                         mutate(Results = round(Results, digits = 0)))
     
-    # Table to show simulation summary
+    # Table to show simulation summary ----
     output$Simulation_Summary <- renderDataTable(
         stock_simulation() %>%
             tibble() %>%
@@ -221,9 +240,20 @@ server <- function(input, output) {
             map_df(.x = ., .f = ~round(., digits = 1))
     )
     
-    # Plot - Histogram
+    # Plot - Histogram ----
     output$plotly_plot <- renderPlotly({
         hist_plotter(df=stock_simulation(), current_price = average_price())
+    })
+    
+    # Plot - Density ----
+    output$plotly_plot_density <- renderPlotly({
+        density_plotter(df=stock_simulation(), current_price = average_price())
+    })
+    
+    # Plot - Time Series ----
+    output$time_series_plot <- renderPlotly({
+        data() %>%
+            plot_time_series(.date_var = Date, .value = Adjusted)
     })
     
     
--- functions.R
+++ functions.R
@@ -1,6 +1,6 @@
 # Monte-Carlo Simulation
 
-stock_price_model <- function(cp, days, gr, vol){
+stock_price_model <- function(cp, days, gr, vol, forecast_days){
   
   # cp - current price  / days - annual trading days / gr - growth rate / vol - annual volatility
   
@@ -12,7 +12,7 @@ stock_price_model <- function(cp, days, gr, vol){
   begin_price <- cp
   
   # stock price in 3 months
-  for(i in 1:90){
+  for(i in 1:forecast_days){
     # generate normally distributed random number
     growth_factor <- rnorm(n = 1, mean = daily_mean, sd = daily_std)
     # calculate ending price
@@ -38,6 +38,26 @@ hist_plotter <- function(df, current_price){
       geom_histogram(show.legend = FALSE, bins = 50) +
       theme_bw() +
       geom_vline(aes(xintercept=mean(value))) +
+      labs(x = "Price of Stock", y= "Count", title = "Stock Price Estimates")
+  )
+}
+
+density_plotter <- function(df, current_price){
+  plotly::ggplotly(
+    df %>%
+      as.tibble() %>%
+      mutate(label = case_when(
+        value > current_price ~ "Buy",
+        TRUE ~ "Not Buy"
+      )) %>%
+      ggplot(aes(value, fill=label)) + 
+      geom_density(show.legend = FALSE, fill = "blue") +
+      theme_bw() +
+      geom_vline(aes(xintercept=mean(value))) +
       labs(x = "Price of Stock", title = "Stock Price Estimates")
   )
 }
+
+
+
+
